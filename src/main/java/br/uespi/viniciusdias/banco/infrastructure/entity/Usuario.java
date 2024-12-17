a1108bb0d79dcfce133648703adedf2d2b30d297
Vinícius Dias <vyvy292004@gmail.com>
2024-12-17 02:50:05 -0300

    feat(Usuario): initial modeling of the user entity

---
+++ src/main/java/br/uespi/viniciusdias/banco/infrastructure/entity/Usuario.java
@@ -0,0 +1,31 @@
+package br.uespi.viniciusdias.banco.infrastructure.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+}
